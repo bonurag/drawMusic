cd423ea36ad5d5c94eed0ac772dfa57293f48e76
Giuseppe <giuseppebonuraster@gmail.com>
2020-03-14 21:08:17 +0100

    Delete method getMaxOfPairPitch, inserito controllo sull'ottava durante il calcolo delle permutazioni

--- drawMusic/src/testMethod.java
+++ drawMusic/src/testMethod.java
@@ -437,17 +437,22 @@ public class testMethod
                 System.out.println("binomialNoteMap: " + binomialNoteMap);
                 
                 notesListPermutation = new ArrayList<>();
+                
                 for(int k=0; k<notesListInChord.size(); k++)
                 {               
                     for(int w=k+1; w<notesListInChord.size(); w++)
                     {
-                        String singlePermutation = notesListInChord.get(k) + ":" + notesListInChord.get(w);
+                        String singlePermutation = "";
+                        Integer cncK = calculateCNC(notesListInChord.get(k));
+                        Integer cncW = calculateCNC(notesListInChord.get(w));
+                        if(cncK > cncW)
+                            singlePermutation = notesListInChord.get(k) + ":" + notesListInChord.get(w);
+                        else
+                            singlePermutation = notesListInChord.get(w) + ":" + notesListInChord.get(k);
                         notesListPermutation.add(singlePermutation);    
                     }
                 }
-                //System.out.println("notesListPermutation: " + notesListPermutation); 
-                
-                getMaxOfPairPitch(notesListPermutation);
+                System.out.println("notesListPermutation: " + notesListPermutation); 
                         
                 for(String intervalKey : notesListPermutation)
                 {
@@ -480,42 +485,19 @@ public class testMethod
         }
     }
     
-    //Ritorna la lista del massimo valore tra coppie di Pitch
-    public static ArrayList<Integer> getMaxOfPairPitch(ArrayList<String> inputPitchList)
-    {
-        String[] tempVar;
-        ArrayList<Integer> pairPitch = new ArrayList<>();
-        ArrayList<Integer> maxPairPitchValue = new ArrayList<>();
-        if(inputPitchList.size() > 0)
-        {
-            for(int j=0; j<inputPitchList.size(); j++)
-            {
-                Integer maxValue = 0;
-                pairPitch = new ArrayList<>();
-                tempVar = inputPitchList.get(j).split(":");
-                pairPitch.add(calculateCNC(tempVar[0]));
-                pairPitch.add(calculateCNC(tempVar[1]));
-                maxValue = Collections.max(pairPitch);
-                maxPairPitchValue.add(maxValue);
-            }
-            System.out.println("Inside getMaxOfPairPitch parameter: maxPairPitchValue - value: " + maxPairPitchValue);     
-        }
-
-        return maxPairPitchValue;
-    }
-    
+    //Metodo che calcola il CONTINUOUS NAME CODE passanto in input un pitch
     public static Integer calculateCNC(String inputNote)
     {
         Integer octave = Integer.parseInt(inputNote.substring(inputNote.length()-1, inputNote.length()));
         Integer nameClass = getNameClass(inputNote.substring(0, 1));
-        System.out.println("Inside calculateCNC parameter: octave - value: " + octave);
-        System.out.println("Inside calculateCNC parameter: nameClass - value: " + nameClass);
+        //System.out.println("Inside calculateCNC parameter: octave - value: " + octave);
+        //System.out.println("Inside calculateCNC parameter: nameClass - value: " + nameClass);
         return (octave * 7) + nameClass;        
     }
     
     public static Integer getPitchClass(String inputNote)
     {
-        System.out.println("Inside getPitchClass inputNote: " + inputNote);
+        //System.out.println("Inside getPitchClass inputNote: " + inputNote);
         
         Integer pitchClassValue = -1;
         ArrayList<String> pitch0 = new ArrayList<>(Arrays.asList("C","B#","Dbb"));
@@ -555,7 +537,7 @@ public class testMethod
     
     public static Integer getNameClass(String inputNote)
     {
-        System.out.println("Inside getNameClass inputNote: " + inputNote);
+        //System.out.println("Inside getNameClass inputNote: " + inputNote);
         
         Integer nameClassValue = -1;
         String nameClass0 = "C";
